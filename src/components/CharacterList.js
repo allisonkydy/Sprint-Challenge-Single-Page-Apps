87dc25369e9f38d2cd4d6fa7698f3729f37e3aa4
allisonkydy <allisnelly@gmail.com>
2019-08-23 12:49:31 -0500

    add character pagination

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,28 +1,45 @@
 import React, { useEffect, useState } from "react";
 import axios from 'axios';
 import CharacterCard from "./CharacterCard";
+import { Pagination } from "semantic-ui-react";
 
 export default function CharacterList() {
   const [characters, setCharacters] = useState({});
-  // data is in characters.results (array)
-  // pages is in characters.info (object)
+  const [page, setPage] = useState(1);
+
+  const changePage = (e, { activePage }) => {
+    setPage(activePage);
+  }
 
   useEffect(() => {
-    axios.get('https://rickandmortyapi.com/api/character/')
+    axios.get(`https://rickandmortyapi.com/api/character/?page=${page}`)
       .then(response => {
         console.log(response);
         setCharacters(response.data);
       })
       .catch(error => console.log(error))
-  }, []);
+  }, [page]);
 
   if (!characters.results) return <h2>Loading...</h2>
 
   return (
-    <section className="character-list grid-view">
-      {characters.results.map(character => {
-        return <CharacterCard character={character} key={character.id}/>
-      })}
-    </section>
+    <>
+      <Pagination
+        boundaryRange={0}
+        defaultActivePage={1}
+        activePage={page}
+        onPageChange={changePage}
+        ellipsisItem={null}
+        firstItem={null}
+        lastItem={null}
+        siblingRange={1}
+        totalPages={25}
+      />
+      <section className="character-list grid-view">
+        {characters.results.map(character => {
+          return <CharacterCard character={character} key={character.id}/>
+        })}
+      </section>
+    </>
   );
 }
