f2097588b177f788ae05ad9a9188df3e5b178e66
allisonkydy <allisnelly@gmail.com>
2019-08-23 10:50:49 -0500

    location cards built and rendered

--- src/components/LocationsList.js
+++ src/components/LocationsList.js
@@ -1,3 +1,29 @@
 import React, { useEffect, useState } from "react";
+import axios from "axios";
 
-export default function LocationsList() {}
+import LocationCard from "./LocationCard";
+
+export default function LocationsList() {
+   const [locations, setLocations] = useState({});
+   // data is in locations.results (array)
+   // pages is in locations.info (object)
+
+   useEffect(() => {
+      axios.get('https://rickandmortyapi.com/api/location/')
+         .then(response => {
+            console.log(response);
+            setLocations(response.data);
+         })
+         .catch(error => console.log(error))
+   }, [])
+
+  if (!locations.results) return <h2>Loading...</h2>
+
+   return (
+      <section>
+         {locations.results.map(location => {
+            return <LocationCard location={location} />
+         })}
+      </section>
+   )
+}
--- src/components/TabNav.js
+++ src/components/TabNav.js
@@ -2,13 +2,15 @@ import React from "react";
 import { Tab, Menu, Icon } from "semantic-ui-react";
 import { NavLink } from "react-router-dom";
 import CharacterList from "./CharacterList";
+import LocationsList from "./LocationsList";
 
 // TODO: Add missing tabs below
 // Take a look at React Semantic UI tabs 
 // https://react.semantic-ui.com/modules/tab/
 export default function TabNav() {
    return (
-      <CharacterList />
+      // <CharacterList />
+      <LocationsList />
    )
 };
 
