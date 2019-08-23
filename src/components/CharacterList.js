b925ef5e312430326034fcbc1d298464acd8292e
allisonkydy <allisnelly@gmail.com>
2019-08-23 10:28:29 -0500

    hooked up CharacterList, set state and fetched character data

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,23 @@
 import React, { useEffect, useState } from "react";
+import axios from 'axios';
 
 export default function CharacterList() {
-  // TODO: Add useState to track data from useEffect
+  const [characters, setCharacters] = useState({});
+  // data is in characters.results (array)
+  // pages is in characters.info (object)
 
   useEffect(() => {
-    // TODO: Add API Request here - must run in `useEffect`
-    //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+    axios.get('https://rickandmortyapi.com/api/character/')
+      .then(response => {
+        console.log(response);
+        setCharacters(response.data);
+      })
+      .catch(error => console.log(error))
   }, []);
 
   return (
     <section className="character-list grid-view">
-      <h2>TODO: `array.map()` over your state here!</h2>
+      {/* <h2>TODO: `array.map()` over your state here!</h2> */}
     </section>
   );
 }
--- src/components/TabNav.js
+++ src/components/TabNav.js
@@ -1,11 +1,14 @@
 import React from "react";
 import { Tab, Menu, Icon } from "semantic-ui-react";
 import { NavLink } from "react-router-dom";
+import CharacterList from "./CharacterList";
 
 // TODO: Add missing tabs below
 // Take a look at React Semantic UI tabs 
 // https://react.semantic-ui.com/modules/tab/
 export default function TabNav() {
-
+   return (
+      <CharacterList />
+   )
 };
 
